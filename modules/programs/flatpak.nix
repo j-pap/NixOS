0beb6495e08991ef10168b690b4105d9096a9918
j-pap <205946337+j-pap@users.noreply.github.com>
2025-04-17 14:32:24 -0500

    flatpak: update formatting

--- modules/programs/flatpak.nix
+++ modules/programs/flatpak.nix
@@ -6,36 +6,32 @@
   ...
 }: let
   cfg = cfgOpts.flatpak;
-  flatseal = lib.mkIf (cfgOpts.desktops.gnome.enable) "com.github.tchx84.Flatseal";
+  # Search packages via 'https://flathub.org/apps/search'
+  flatPaks = [
+    #"org.libreoffice.LibreOffice"
+  ] ++ lib.optional (cfgOpts.desktops.gnome.enable) [
+    "com.github.tchx84.Flatseal"
+  ];
 in {
   imports = [ inputs.nix-flatpak.nixosModules.nix-flatpak ];
 
   options.myOptions.flatpak.enable = lib.mkEnableOption "Flatpak";
 
   config = lib.mkIf (cfg.enable) {
-    environment.systemPackages = (
-      lib.optional (cfgOpts.desktops.gnome.enable) [
-        pkgs.gnome-software             # Gnome store
-      ]
-    ) ++ (
-      lib.optional (cfgOpts.desktops.kde.enable) [
-        pkgs.kdePackages.discover       # KDE store
-        pkgs.kdePackages.flatpak-kcm    # Flatpak KDE settings module
-      ]
-    );
+    environment.systemPackages = (lib.optional (cfgOpts.desktops.gnome.enable) [
+      pkgs.gnome-software             # Gnome store
+    ]) ++ (lib.optional (cfgOpts.desktops.kde.enable) [
+      pkgs.kdePackages.discover       # KDE store
+      pkgs.kdePackages.flatpak-kcm    # Flatpak KDE settings module
+    ]);
 
     services.flatpak = {
       enable = true;
-      # Search package names via https://flathub.org/apps/search?q=
-      packages = [
-        flatseal
-        #"org.libreoffice.LibreOffice"
-      ];
-      # Flathub added by default
-      remotes = [{
-        #name = "flathub";
-        #location = "https://dl.flathub.org/repo/flathub.flatpakrepo";
-      }];
+      packages = flatPaks;
+      remotes = lib.singleton {
+        name = "flathub";
+        location = "https://dl.flathub.org/repo/flathub.flatpakrepo";
+      };
       uninstallUnmanaged = false;
       update.auto = {
         enable = true;
