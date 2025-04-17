02cc6bedc5cb8b3eab5149230796743283043af1
j-pap <205946337+j-pap@users.noreply.github.com>
2025-04-17 14:09:00 -0500

    bash: adjust aliases

--- common/programs/bash.nix
+++ common/programs/bash.nix
@@ -14,14 +14,8 @@
       "exifstrip" = "exiftool -All= \"$@\"";
       "ff" = "fastfetch";
       "fishy" = "asciiquarium";
-      "ga" = "git add";
-      "gc" = "git commit";
-      "gd" = "git diff";
-      "gs" = "git status";
       "ll" = "ls -la";
-      "nixdiff" = "nixos-rebuild build && nix store diff-closures /run/current-system ./result";
-      "ns" = "nix search nixpkgs#\"$1\"";
-      "spf" = "superfile";
+      "nix diff" = "nixos-rebuild build && nix store diff-closures /run/current-system ./result";
     };
   };
 }
