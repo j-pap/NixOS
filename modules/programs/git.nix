9e7d8af95f5ebaae5b8f7c81df9dc77824df20f5
j-pap <205946337+j-pap@users.noreply.github.com>
2025-07-26 07:49:02 -0500

    git: add assertions & switch matchblocks to list

--- modules/programs/git.nix
+++ modules/programs/git.nix
@@ -14,6 +14,21 @@ in {
   };
 
   config = {
+    assertions = [
+      {
+        assertion = !(cfg.libsecret.enable && cfg.oauth.enable);
+        message = "(Git) Libsecret and OAuth may not be used at the same time.";
+      }
+      {
+        assertion = !(cfg.libsecret.enable && cfg.ssh.enable);
+        message = "(Git) Libsecret and SSH may not be used at the same time.";
+      }
+      {
+        assertion = !(cfg.oauth.enable && cfg.ssh.enable);
+        message = "(Git) OAuth and SSH may not be used at the same time.";
+      }
+    ];
+
     myOptions.git.ssh.enable = lib.mkDefault true;
 
     home-manager.users.${myUser} = lib.mkMerge [
@@ -44,36 +59,36 @@ in {
             gpg = {
               format = "ssh";
               ssh.program = (
-                if (cfgOpts."1password".enable)
-                  then (lib.getExe' pkgs._1password-gui "op-ssh-sign")
-                else (lib.getExe' pkgs.openssh "ssh")
+                if (cfgOpts."1password".enable) then
+                  (lib.getExe' pkgs._1password-gui "op-ssh-sign")
+                else
+                  (lib.getExe' pkgs.openssh "ssh-keygen")
               );
             };
             user.signingkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINdsPgO+W30YwojR6rmyFQ7JOoracCgncClxVUAkTNoJ";
           };
 
           ssh = let
+            hosts = [
+              "FW13"
+              "T1"
+              "T450s"
+            ];
             identityAgent = (
-              if (cfgOpts."1password".enable)
-                then "/home/${myUser}/.1password/agent.sock"
-              else (lib.getExe' pkgs.openssh "ssh-agent")
+              if (cfgOpts."1password".enable) then
+                "/home/${myUser}/.1password/agent.sock"
+              else
+                (lib.getExe' pkgs.openssh "ssh-agent")
             );
           in {
             enable = true;
-            matchBlocks = {
-              "FW13" = {
-                extraOptions.IdentityAgent = identityAgent;
+            matchBlocks = builtins.listToAttrs (builtins.map (host: {
+              name = host;
+              value = {
+                identityAgent = identityAgent;
                 forwardAgent = true;
               };
-              "T1" = {
-                extraOptions.IdentityAgent = identityAgent;
-                forwardAgent = true;
-              };
-              "T450s" = {
-                extraOptions.IdentityAgent = identityAgent;
-                forwardAgent = true;
-              };
-
+            }) hosts) // {
               "github.com".match = ''
                 host github.com exec "test -z $SSH_TTY"
                   IdentityAgent ${identityAgent}
