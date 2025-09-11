a6eea7958f85dfe67400ea33a37d754b837553d5
j-pap <205946337+j-pap@users.noreply.github.com>
2025-09-10 23:38:17 -0500

    containers: update logic & settings

--- modules/programs/containers.nix
+++ modules/programs/containers.nix
@@ -1,4 +1,5 @@
 {
+  config,
   lib,
   pkgs,
   cfgOpts,
@@ -7,43 +8,78 @@
 }: let
   cfg = cfgOpts.containers;
 in {
-  options.myOptions.containers.enable = lib.mkEnableOption "Containers";
-
-  config = lib.mkIf (cfg.enable) {
-    environment.systemPackages = builtins.attrValues {
-      inherit (pkgs)
-      # Kubernetes Tools
-        kind
-        kubectl
-      ;
-    };
-
-    users.users.${myUser}.extraGroups = [
-      #"docker" # Grants root socket access - use docker.setSocketVariable instead
-      "podman"
-    ];
-
-    # Docker
-    virtualisation = {
-      # Docker
-      docker = {
-        enable = true;
-        rootless = {
-          enable = true;
-          setSocketVariable = true;
-        };
-        storageDriver = "btrfs";
-      };
-      #oci-containers.backend = "docker";
-
-      # Podman
-      containers.enable = true;
-      #oci-containers.backend = "podman";
-      podman = {
-        enable = true;
-        #dockerCompat = true; # Docker alias
-        defaultNetwork.settings.dns_enabled = true;
-      };
-    };
+  options.myOptions.containers = {
+    enable = lib.mkEnableOption "Containers";
+    docker = lib.mkEnableOption "Docker";
+    podman = lib.mkEnableOption "Podman";
   };
+
+  config = lib.mkMerge [
+    (lib.mkIf (cfg.enable) {
+      assertions = [
+        {
+          assertion = !(cfg.docker && cfg.podman);
+          message = "Docker and Podman may not be used at the same time.";
+        }
+      ];
+
+      myOptions.containers.docker = lib.mkDefault true;
+
+      environment.systemPackages = builtins.attrValues {
+        inherit (pkgs)
+        # Docker
+          compose2nix
+        # Kubernetes Tools
+          #kind
+          #kubectl
+        ;
+      };
+
+      virtualisation.containers.enable = true;
+    })
+
+    (lib.mkIf (cfg.enable && cfg.docker) {
+      users.users.${myUser}.extraGroups = [
+        "docker"
+      ];
+
+      virtualisation = {
+        oci-containers.backend = "docker";
+        docker = {
+          enable = true;
+          autoPrune = { };
+          daemon.settings = {
+            data-root = "/var/lib/docker";
+            #default-address-pools = [ ];
+            #dns = [ ];
+            ipv6 = lib.mkIf (config.networking.enableIPv6) true;
+            live-restore = true;
+            log-driver = "journald";
+            storage-driver = "btrfs";
+          };
+          rootless = {
+            #enable = true;
+            #daemon.settings = { };
+            #setSocketVariable = true;
+          };
+        };
+      };
+    })
+
+    (lib.mkIf (cfg.enable && cfg.podman) {
+      users.users.${myUser}.extraGroups = [
+        "podman"
+      ];
+
+      virtualisation = {
+        oci-containers.backend = "podman";
+        podman = {
+          enable = true;
+          autoPrune = { };
+          dockerCompat = true; # Docker alias
+          defaultNetwork.settings.dns_enabled = true;
+        };
+      };
+    })
+  ];
 }
