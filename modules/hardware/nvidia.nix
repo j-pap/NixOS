3b0538a7e0eae10ce55e916b92b58a9237922061
j-pap <205946337+j-pap@users.noreply.github.com>
2025-10-28 20:02:02 -0500

    nvidia: clean up formatting

--- modules/hardware/nvidia.nix
+++ modules/hardware/nvidia.nix
@@ -7,6 +7,7 @@
   ...
 }: let
   cfg = cfgOpts.hardware.nvidia;
+  nvidiaPkg = "stable"; # or beta
 in {
   options.myOptions.hardware.nvidia.enable = lib.mkEnableOption "Nvidia GPU";
 
@@ -18,8 +19,7 @@ in {
       ];
 
       environment = {
-        # Suppress Firefox's libva logging
-        etc."libva.conf".text = "LIBVA_MESSAGING_LEVEL=1";
+        etc."libva.conf".text = "LIBVA_MESSAGING_LEVEL=1";  # Suppress Firefox's libva logging
         systemPackages = [
           pkgs.egl-wayland
           pkgs.nvtopPackages.nvidia
@@ -28,41 +28,33 @@ in {
           __GL_GSYNC_ALLOWED = 1;
           __GL_VRR_ALLOWED = 1;
           __GLX_VENDOR_LIBRARY_NAME = "nvidia";
-          # GBM_BACKEND could possibily cause Firefox to crash - comment out if so
-          GBM_BACKEND = "nvidia-drm";
-          # Hardware Accelaration - 'nvidia' or 'vdpau'
-          LIBVA_DRIVER_NAME = "nvidia";
-          # Disables the Firefox sandbox for the RDD process that the decoder runs in
-          MOZ_DISABLE_RDD_SANDBOX = 1;
-          # Library backend - 'direct' or 'egl'
-          NVD_BACKEND = "direct";
+          GBM_BACKEND = "nvidia-drm";  # Could possibily cause Firefox to crash - comment out if so
+          LIBVA_DRIVER_NAME = "nvidia";  # Hardware Acceleration - 'nvidia' or 'vdpau'
+          MOZ_DISABLE_RDD_SANDBOX = 1;  # Disables Firefox's sandbox for the RDD process that the decoder runs in
+          NVD_BACKEND = "direct";  # Library backend - 'direct' or 'egl'
         };
       };
 
       hardware = {
         graphics.enable = true;
         nvidia = {
-          # "nvidia-drm.modeset=1" / "nvidia-drm.fbdev=1" enables dedicated framebuffer
-          modesetting.enable = true;
+          modesetting.enable = true;  # "nvidia-drm.modeset=1" / "nvidia-drm.fbdev=1" enables dedicated framebuffer
           nvidiaSettings = true;
-          # Starting w/ 560, open drivers are used by default
-          open = false;
-          # beta or stable
-          package = config.boot.kernelPackages.nvidiaPackages.stable.overrideAttrs (old: {
-            buildInputs = (old.buildInputs or [ ]) ++ [ pkgs.bash ]; # Patches nvidia-sleep.sh via patchShebangs
+          open = false;  # Starting w/ 560, open drivers are used by default
+          package = config.boot.kernelPackages.nvidiaPackages.${nvidiaPkg}.overrideAttrs (old: {
+            buildInputs = (old.buildInputs or [ ]) ++ [ pkgs.bash ];  # Patches nvidia-sleep.sh via patchShebangs
           });
           powerManagement = {
             # "nvidia.NVreg_PreserveVideoMemoryAllocations=1" / enables nvidia-hibernate/resume/suspend.services
               # enable if graphical corruption on resumption from suspend
             enable = true;
-            # Experimental: Turns off GPU when not in use - cannot be used w/ nvidia.prime.sync
-            finegrained = false;
+            finegrained = false;  # Experimental: Turns off GPU when not in use - cannot be used w/ nvidia.prime.sync
           };
-          # nvidia-vaapi-driver
-          videoAcceleration = true;
+          videoAcceleration = true;  # nvidia-vaapi-driver
         };
       };
 
+      # Firefox about:config(s)
       home-manager.users.${myUser}.programs.${cfgOpts.browser}.profiles.${myUser}.settings = {
         "gfx.x11-egl.force-enabled" = true;
         "media.rdd-ffmpeg.enabled" = true;
@@ -76,8 +68,9 @@ in {
     #(lib.mkIf (cfg.enable && cfgOpts.desktops.hyprland.enable) { })
 
     (lib.mkIf (cfg.enable && cfgOpts.desktops.kde.enable) {
-      # Disable GSP Mode - Smoother Plasma Wayland experience
-      boot.kernelParams = [ "nvidia.NVreg_EnableGpuFirmware=0" ];
+      boot.kernelParams = [
+        "nvidia.NVreg_EnableGpuFirmware=0"  # Disable GSP Mode - Smoother Plasma Wayland experience
+      ];
     })
   ];
 }
