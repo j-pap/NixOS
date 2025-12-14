4d9d2ba8285e178267b9885171636ef608e4dbe8
j-pap <205946337+j-pap@users.noreply.github.com>
2025-12-13 20:11:56 -0600

    bash: update aliases

--- common/programs/bash.nix
+++ common/programs/bash.nix
@@ -5,18 +5,33 @@
   home-manager.users.${myUser}.programs.bash = {
     enable = true;
     shellAliases = {
+      # Directories
       ".." = "cd ..";
+      "..." = "cd ../..";
+      "...." = "cd ../../..";
       ".df" = "cd /etc/nixos";
+      "ls" = "eza --long --header --group-directories-first --links --group --modified --git --icons";
+      "lsa" = "eza --all --long --header --group-directories-first --links --group --modified --git --icons";
+      "lt" = "eza --tree";
+      "lta" = "eza --tree --all";
+
+      # Misc
       "bonsai" = "cbonsai --screensaver";
+      "ff" = "fastfetch";
+      "n" = "nvim";
+      "swim" = "asciiquarium";
+
+      # Network
+      "showdns" = "nmcli device show \"$@\" | grep DNS";
+
+      # Nix
+      "nix-diff" = "nix store diff-closures /run/current-system \"$@\"";
+      "nix-eval" = "cd /etc/nix/nixpkgs && nix repl .";
+      "nix-host" = "nixos-rebuild build --build-host \"$@\"";
+
+      # Pictures
       "exifinfo" = "exiftool -a -g -s \"$@\"";
       "exifstrip" = "exiftool -All= \"$@\"";
-      "ff" = "fastfetch";
-      "fishy" = "asciiquarium";
-      "ll" = "eza --long --all --header --links --group --modified --git --icons";
-      "nix-diff" = "nix store diff-closures /run/current-system \"$@\"";
-      "nix-host" = "nixos-rebuild build --build-host \"$@\"";
-      "showdns" = "nmcli device show \"$@\" | grep DNS";
-      "tree" = "eza --tree --all";
     };
   };
 }
