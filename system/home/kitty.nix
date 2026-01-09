f93048f5d50f12af6fdc6175498c23d0e106099b
j-pap <205946337+j-pap@users.noreply.github.com>
2026-01-08 23:11:57 -0600

    kitty: focus follows mouse

--- system/home/kitty.nix
+++ system/home/kitty.nix
@@ -34,6 +34,7 @@ in
         dim_opacity = "0.5"; # Dim/faint font's opacity
         dynamic_background_opacity = "yes";
         enable_audio_bell = "no";
+        focus_follows_mouse = "yes";
         linux_display_server = "wayland";
         symbol_map = "U+e000-U+e00a,U+ea60-U+ebeb,U+e0a0-U+e0c8,U+e0ca,U+e0cc-U+e0d7,U+e200-U+e2a9,U+e300-U+e3e3,U+e5fa-U+e6b1,U+e700-U+e7c5,U+ed00-U+efc1,U+f000-U+f2ff,U+f000-U+f2e0,U+f300-U+f372,U+f400-U+f533,U+f0001-U+f1af0 Symbols Nerd Font Mono";
         tab_bar_edge = "top";
