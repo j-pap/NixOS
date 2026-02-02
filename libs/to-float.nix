c9e71d8fc016f9785a944a6b663b0be2ef5aa6c5
j-pap <205946337+j-pap@users.noreply.github.com>
2026-02-02 13:53:15 -0600

    libs: add function descriptions

--- libs/to-float.nix
+++ libs/to-float.nix
@@ -1,17 +1,29 @@
+#######################################
+# Convert a string to a float (trimmed to hundredths)
+# Globals:
+#   parse: convert string to int
+#   w: left digits of dec's decimal point, a string
+#   f: right digits of dec's decimal point, a string
+#   whole: w converted, an integer
+#   frac: f converted, an integer to a float
+# Arguments:
+#   dec: string to convert
+# Returns:
+#   "1.123456" -> 1.12
+#######################################
 let
   matchStripInput = builtins.match "[[:space:]]*0*(-?[[:digit:]]+)[[:space:]]*";
   matchZero = builtins.match "0+";
 
-  strCheck = s: matchStripInput s;
-  isZero = s: matchZero (builtins.head (strCheck s)) == [ ];
-  strParsed = s: builtins.fromJSON (builtins.head (strCheck s));
+  check = s: matchStripInput s;
+  isZero = s: matchZero (builtins.head (check s)) == [ ];
+  parse = s: builtins.fromJSON (builtins.head (check s));
 in
-str:
+dec:
 let
-  wStr = builtins.elemAt (builtins.splitVersion str) 0;
-  dStr = builtins.substring 0 2 (builtins.elemAt (builtins.splitVersion str) 1);
-
-  whole = if (isZero wStr) then 0 else (strParsed wStr);
-  decimal = if (isZero dStr) then 0 else (strParsed dStr);
+  w = builtins.elemAt (builtins.splitVersion dec) 0;
+  f = builtins.substring 0 2 (builtins.elemAt (builtins.splitVersion dec) 1);
+  whole = if (isZero w) then 0 else (parse w);
+  frac = (if (isZero f) then 0 else (parse f)) / 100.0;
 in
-whole + (decimal / 100.0)
+whole + frac
--- libs/truncate-float.nix
+++ libs/truncate-float.nix
@@ -1,6 +1,17 @@
-f: p:
+#######################################
+# Trim a float to the specified number of places
+# Globals:
+#   trunc: trims the unwanted digits from the result, a float
+#   power: multiply 'n' by itself for 'e' number of times, an integer or float
+# Arguments:
+#   float: decimal to trim, a float
+#   place: number of digits to remain, an integer
+# Returns:
+#   1.123456 3 -> 1.123
+#######################################
+float: place:
 let
-  trunc = f: if f < 0 then builtins.ceil f else builtins.floor f;
-  pow = b: n: builtins.foldl' builtins.mul 1 (builtins.genList (_: b) n);
+  trunc = f: if (f < 0) then builtins.ceil f else builtins.floor f;
+  power = n: e: builtins.foldl' builtins.mul 1 (builtins.genList (_: n) e);
 in
-trunc (f * pow 10 p) / pow 10.0 p
+trunc (float * (power 10 place)) / (power 10.0 place)
