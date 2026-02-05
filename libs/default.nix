62123c8d2cf88c9221fd244698336a41441b0cc6
j-pap <205946337+j-pap@users.noreply.github.com>
2026-02-05 16:13:29 -0600

    libs: inherit lib to remove redundant code

--- libs/default.nix
+++ libs/default.nix
@@ -1,6 +1,10 @@
 {
+  lib,
+  ...
+}:
+{
   _module.args.extraLibs = {
-    toFloat = import ./to-float.nix;
+    toFloat = import ./to-float.nix { inherit lib; };
     truncateFloat = import ./truncate-float.nix;
   };
 }
--- libs/to-float.nix
+++ libs/to-float.nix
@@ -1,7 +1,6 @@
 #######################################
 # Convert a string to a float (trimmed to hundredths)
 # Globals:
-#   parse: convert string to int
 #   w: left digits of dec's decimal point, a string
 #   f: right digits of dec's decimal point, a string
 #   whole: w converted, an integer
@@ -11,19 +10,14 @@
 # Returns:
 #   "1.123456" -> 1.12
 #######################################
-let
-  matchStripInput = builtins.match "[[:space:]]*0*(-?[[:digit:]]+)[[:space:]]*";
-  matchZero = builtins.match "0+";
-
-  check = s: matchStripInput s;
-  isZero = s: matchZero (builtins.head (check s)) == [ ];
-  parse = s: builtins.fromJSON (builtins.head (check s));
-in
+{
+  lib,
+}:
 dec:
 let
-  w = builtins.elemAt (builtins.splitVersion dec) 0;
-  f = builtins.substring 0 2 (builtins.elemAt (builtins.splitVersion dec) 1);
-  whole = if (isZero w) then 0 else (parse w);
-  frac = (if (isZero f) then 0 else (parse f)) / 100.0;
+  w = lib.versions.major dec;
+  f = builtins.substring 0 2 (lib.versions.minor dec);
+  whole = lib.toIntBase10 w;
+  frac = (lib.toIntBase10 f) / 100.0;
 in
 whole + frac
