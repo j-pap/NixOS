04e8c88006cd558e08beae5783915736de8ef5d0
j-pap <205946337+j-pap@users.noreply.github.com>
2026-02-14 08:07:28 -0600

    openrgb: 1.0rc2-pipeline is actually what works
    I had built this first, then switched to the standard 1.0rc2 without rebooting, so assumed that version also worked without overriding

--- modules/programs/openrgb.nix
+++ modules/programs/openrgb.nix
@@ -18,7 +18,16 @@ in
 
     services.hardware.openrgb = {
       enable = true;
-      package = pkgs.openrgb;
+      package = pkgs.openrgb.overrideAttrs (super: {
+        version = "1.0rc2-pipeline-2026-02-13";
+        src = pkgs.fetchFromGitLab {
+          owner = "CalcProgrammer1";
+          repo = "OpenRGB";
+          rev = "880dc1450d0bcd27eec2bc9526590ffb40e87e78";
+          sha256 = "sha256-TsMOujVI178r887IIh7fTPJRKxT3VIi965mipZcimzI=";
+        };
+        patches = [ (builtins.elemAt super.patches 0) ]; # Source's fetchpatch doesn't build with pipeline
+      });
       #server.port = 6742;
       startupProfile = config.networking.hostName;
     };
