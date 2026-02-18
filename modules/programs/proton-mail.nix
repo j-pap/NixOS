b79ddafd91b1cc7f1da97ada0d804b1d7010a04a
j-pap <205946337+j-pap@users.noreply.github.com>
2026-02-18 11:04:22 -0600

    protonmail: update autostart

--- modules/programs/proton-mail.nix
+++ modules/programs/proton-mail.nix
@@ -7,13 +7,13 @@
 }:
 let
   cfg = config.flake.protonmail;
-  protonMB = pkgs.protonmail-bridge-gui; # pkgs or pkgs.stable
+  protonMbPkg = pkgs.protonmail-bridge-gui; # pkgs or pkgs.stable
 in
 {
   options.flake.protonmail.enable = lib.mkEnableOption "Proton Mail Bridge";
 
   config = lib.mkIf (cfg.enable) {
-    environment.systemPackages = [ protonMB ];
+    environment.systemPackages = [ protonMbPkg ];
 
     home-manager.users.${flk.user} = lib.mkMerge [
       # Autostart via Hyprland
@@ -25,17 +25,17 @@ in
       (lib.mkIf (!flk.de.hyprland.enable) {
         xdg.configFile."autostart/ProtonMailBridge.desktop".text = lib.concatLines [
           (lib.replaceStrings [ "Exec=protonmail-bridge-gui" ] [ "Exec=protonmail-bridge-gui --no-window" ] (
-            lib.fileContents "${protonMB}/share/applications/proton-bridge-gui.desktop"
+            lib.fileContents "${protonMbPkg}/share/applications/proton-bridge-gui.desktop"
           ))
-          "X-GNOME-Autostart-enabled=true"
+          (lib.optionalString (flk.de.gnome.enable) "X-GNOME-Autostart-enabled=true")
         ];
       })
     ];
 
     services.protonmail-bridge = {
       enable = false;
-      package = pkgs.protonmail-bridge; # pkgs or pkgs.stable or protonMB
-      logLevel = null; # debug, info, warn, error, fatal, panic
+      package = pkgs.protonmail-bridge; # pkgs, pkgs.stable, or protonMbPkg
+      logLevel = null; # null, debug, info, warn, error, fatal, panic
       path = [ ];
     };
   };
