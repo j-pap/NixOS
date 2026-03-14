76b71bc3127266a43005e58694cfd575a9d2eb1d
j-pap <205946337+j-pap@users.noreply.github.com>
2026-03-13 19:49:11 -0500

    nixvim: add tree-sitter-cli

--- base/home/nixvim/plugins/treesitter.nix
+++ base/home/nixvim/plugins/treesitter.nix
@@ -4,62 +4,70 @@
 }:
 {
   # Highlight, edit, and navigate code
-  programs.nixvim.plugins.treesitter = {
-    enable = true;
-
-    grammarPackages = builtins.attrValues {
-      inherit (pkgs.vimPlugins.nvim-treesitter.builtGrammars)
-        bash
-        css
-        dockerfile
-        go
-        html
-        json
-        lua
-        markdown
-        markdown_inline
-        nix
-        php
-        printf
-        python
-        regex
-        sql
-        terraform
-        toml
-        vim
-        vimdoc
-        xml
-        yaml
+  programs.nixvim = {
+    extraPackages = builtins.attrValues {
+      inherit (pkgs)
+        tree-sitter
         ;
     };
-    # Whether to install grammars defined in grammarPackages
-    nixGrammars = true;
 
-    settings = {
-      # Autoinstall languages that are not installed
-      #auto_install = true;
-      # Not needed when using grammarPackages/nixGrammers
-      #ensure_install = [ ];
+    plugins.treesitter = {
+      enable = true;
 
-      highlight = {
-        enable = true;
-        # Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-        # If you are experiencing weird indenting issues, add the language to
-        # the list of additional_vim_regex_highlighting and disabled languages for indent.
-        additional_vim_regex_highlighting = true;
-        disable = [
-          #ruby
-        ];
+      grammarPackages = builtins.attrValues {
+        inherit (pkgs.vimPlugins.nvim-treesitter.builtGrammars)
+          bash
+          css
+          dockerfile
+          go
+          html
+          json
+          lua
+          markdown
+          markdown_inline
+          nix
+          php
+          printf
+          python
+          regex
+          sql
+          terraform
+          toml
+          vim
+          vimdoc
+          xml
+          yaml
+          ;
+      };
+      # Whether to install grammars defined in grammarPackages
+      nixGrammars = true;
+
+      settings = {
+        # Autoinstall languages that are not installed
+        #auto_install = true;
+        # Not needed when using grammarPackages/nixGrammers
+        #ensure_install = [ ];
+
+        highlight = {
+          enable = true;
+          # Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
+          # If you are experiencing weird indenting issues, add the language to
+          # the list of additional_vim_regex_highlighting and disabled languages for indent.
+          additional_vim_regex_highlighting = true;
+          disable = [
+            #ruby
+          ];
+        };
+
+        indent.enable = true;
       };
 
-      indent.enable = true;
+      # There are additional nvim-treesitter modules that you can use to interact
+      # w/ nvim-treesitter. You should go explore a few and see what interests you:
+      #
+      # - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
+      # - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
+      # - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
     };
-
-    # There are additional nvim-treesitter modules that you can use to interact
-    # w/ nvim-treesitter. You should go explore a few and see what interests you:
-    #
-    # - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-    # - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-    # - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
   };
 }
