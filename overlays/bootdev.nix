898456d28285e0cb40a6d91b6f280c099d7fc9e7
j-pap <205946337+j-pap@users.noreply.github.com>
2026-04-15 18:38:40 -0500

    switch bootdev cli to overrideAttrs

--- overlays/bootdev.nix
+++ overlays/bootdev.nix
@@ -1,3 +1,19 @@
 final: prev: {
-  bootdev-cli = prev.callPackage ../pkgs/bootdev { };
+  bootdev-cli = prev.bootdev-cli.overrideAttrs (
+    finalAttrs: oldAttrs: {
+      version = "1.28.0";
+      src = final.fetchFromGitHub {
+        owner = "bootdotdev";
+        repo = "bootdev";
+        rev = "v${finalAttrs.version}";
+        hash = "sha256-sBPId1wEsIG1E+sf+pbqfz0xW0+PHVAoRYTkFLXpWOU=";
+      };
+      /*
+        src = oldAttrs.src // {
+          rev = "v${finalAttrs.version}";
+          hash = "sha256-sBPId1wEsIG1E+sf+pbqfz0xW0+PHVAoRYTkFLXpWOU=";
+        };
+      */
+    }
+  );
 }
