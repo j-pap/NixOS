21fef5718e0aa69324f8201154a1559562e46d65
j-pap <205946337+j-pap@users.noreply.github.com>
2026-04-16 08:49:27 -0500

    bootdev: 1.28.0->1.29.0

--- overlays/bootdev.nix
+++ overlays/bootdev.nix
@@ -1,17 +1,17 @@
 final: prev: {
   bootdev-cli = prev.bootdev-cli.overrideAttrs (
     finalAttrs: oldAttrs: {
-      version = "1.28.0";
+      version = "1.29.0";
       src = final.fetchFromGitHub {
         owner = "bootdotdev";
         repo = "bootdev";
         rev = "v${finalAttrs.version}";
-        hash = "sha256-sBPId1wEsIG1E+sf+pbqfz0xW0+PHVAoRYTkFLXpWOU=";
+        hash = "sha256-i1U1AsFB/z3h/Aj+YSrfi/U1GWUyawfuL2zJiCWWPgI=";
       };
       /*
         src = oldAttrs.src // {
           rev = "v${finalAttrs.version}";
-          hash = "sha256-sBPId1wEsIG1E+sf+pbqfz0xW0+PHVAoRYTkFLXpWOU=";
+          hash = "";
         };
       */
     }
