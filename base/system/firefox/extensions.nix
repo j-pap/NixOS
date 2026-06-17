06c457cf60df63fbca1b670e3ce75bf1c393fb81
j-pap <205946337+j-pap@users.noreply.github.com>
2026-06-17 10:56:40 -0500

    firefox: format extension comments for better readability

--- base/system/firefox/extensions.nix
+++ base/system/firefox/extensions.nix
@@ -8,28 +8,16 @@ let
   };
 in
 builtins.listToAttrs [
-  # 1Password
-  (mkExt "{d634138d-c276-4fc8-924b-40a0ea21d284}")
-  # Augmented Steam
-  (mkExt "{1be309c5-3e4f-4b99-927d-bb500eb4fa88}")
-  # CanvasBlocker
-  (mkExt "CanvasBlocker@kkapsner.de")
-  # Dark Reader
-  (mkExt "addon@darkreader.org")
-  # Firefox Multi-Account Containters
-  (mkExt "@testpilot-containers")
-  # Firefox PWA
-  (mkExt "firefoxpwa@filips.si")
-  # Proton VPN
-  (mkExt "vpn@proton.ch")
-  # SimpleLogin
-  (mkExt "addon@simplelogin")
-  # SponsorBlock
-  (mkExt "sponsorBlocker@ajay.app")
-  # Tabliss
-  (mkExt "extension@tabliss.io")
-  # TTV LOL PRO
-  (mkExt "{76ef94a4-e3d0-4c6f-961a-d38a429a332b}")
-  # uBlock Origin
-  (mkExt "uBlock0@raymondhill.net")
+  (mkExt "{d634138d-c276-4fc8-924b-40a0ea21d284}") # 1Password
+  (mkExt "{1be309c5-3e4f-4b99-927d-bb500eb4fa88}") # Augmented Steam
+  (mkExt "CanvasBlocker@kkapsner.de") # CanvasBlocker
+  (mkExt "addon@darkreader.org") # Dark Reader
+  (mkExt "@testpilot-containers") # Firefox Multi-Account Containters
+  (mkExt "firefoxpwa@filips.si") # Firefox PWA
+  (mkExt "vpn@proton.ch") # Proton VPN
+  (mkExt "addon@simplelogin") # SimpleLogin
+  (mkExt "sponsorBlocker@ajay.app") # SponsorBlock
+  (mkExt "extension@tabliss.io") # Tabliss
+  (mkExt "{76ef94a4-e3d0-4c6f-961a-d38a429a332b}") # TTV LOL PRO
+  (mkExt "uBlock0@raymondhill.net") # uBlock Origin
 ]
