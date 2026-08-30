17392aa8393ae7422bd89dad6fe0aa18d3bb0d6b
j-pap <205946337+j-pap@users.noreply.github.com>
2026-08-30 14:01:45 -0500

    bash: update eza alias

--- base/home/bash.nix
+++ base/home/bash.nix
@@ -9,10 +9,12 @@
       ".df" = "cd /etc/nixos";
       ls = "eza";
       lsa = "eza --all";
-      ll = "eza --long --header --group-directories-first --links --group --modified --git --icons";
-      lla = "eza --all --long --header --group-directories-first --links --group --modified --git --icons";
+      ll = "eza --long --header --group-directories-first --links --group --modified --git --icons always";
+      lla = "eza --long --all --header --group-directories-first --links --group --modified --git --icons always";
       lt = "eza --tree";
       lta = "eza --tree --all";
+      ltl = "eza --tree --long --header --group-directories-first --links --group --modified --git --icons always";
+      ltla = "eza --tree --long --all --header --group-directories-first --links --group --modified --git --icons always";
 
       # Misc
       bonsai = "cbonsai --screensaver";
